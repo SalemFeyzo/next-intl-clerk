ce5c0ba90399ff47b54ace52d351194d99d78ec6
SalemFeyzo <f1989salim@gmail.com>
2024-02-12 19:03:26 +0300

    Environment vars types

---
+++ environment.d.ts
@@ -0,0 +1,12 @@
+//eslint-disable-next-line
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    NODE_ENV: "development" | "production" | "test";
+    SITE_URL: string;
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
+    CLERK_SECRET_KEY: string;
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: string;
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: string;
+    NEXT_PUBLIC_LOCALE_PREFIX: string;
+  }
+}
