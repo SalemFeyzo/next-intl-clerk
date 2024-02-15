a99ed5e5904502641086c6e76ab61d514ec8e838
SalemFeyzo <f1989salim@gmail.com>
2024-02-15 20:27:04 +0300

    Packages Updates And Fixes

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,4 @@
 // Declaring this interface provides type safety for message keys
 type Messages = typeof import("./messages/en.json");
+// eslint-disable-next-line no-unused-vars
 declare interface IntlMessages extends Messages {}
